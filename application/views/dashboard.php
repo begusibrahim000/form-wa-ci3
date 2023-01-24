393f09f8a22a96fa5abfd0bd29f3e98a7bd4983f
begusibrahim000 <begusibrahim000@gmail.com>
2023-01-24 20:38:24 +0700

    form dinamis wa ci3

---
+++ application/views/dashboard.php
@@ -0,0 +1,92 @@
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        <i class="fa fa-tachometer" aria-hidden="true"></i> Dashboard
+        <small>Control panel</small>
+      </h1>
+    </section>
+    
+    <section class="content">
+        <div class="row">
+            <div class="col-lg-3 col-xs-6">
+              <!-- small box -->
+              <div class="small-box bg-aqua">
+                <div class="inner">
+                  <h3>150</h3>
+                  <p>New Tasks</p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-bag"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+              </div>
+            </div><!-- ./col -->
+            <div class="col-lg-3 col-xs-6">
+              <!-- small box -->
+              <div class="small-box bg-green">
+                <div class="inner">
+                  <h3>53<sup style="font-size: 20px">%</sup></h3>
+                  <p>Completed Tasks</p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-stats-bars"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+              </div>
+            </div><!-- ./col -->
+            <div class="col-lg-3 col-xs-6">
+              <!-- small box -->
+              <div class="small-box bg-yellow">
+                <div class="inner">
+                  <h3>44</h3>
+                  <p>New User</p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-person-add"></i>
+                </div>
+                <a href="<?php echo base_url(); ?>userListing" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+              </div>
+            </div><!-- ./col -->
+            <div class="col-lg-3 col-xs-6">
+              <!-- small box -->
+              <div class="small-box bg-red">
+                <div class="inner">
+                  <h3>65</h3>
+                  <p>Reopened Issue</p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-pie-graph"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+              </div>
+            </div><!-- ./col -->
+          </div>
+
+          <div class="row">
+            <div class="col-md-6">
+               <form class="form">
+                <h1>*ULPLTD-MG Tarakan*</h1>
+                <div class="form-group">
+                  <input type="text" class="form-control" id="" placeholder="Wartsila #1 (MG)">
+                </div>
+                <div class="form-group">
+                  <input type="text" class="form-control" id="" placeholder="Wartsila #2 (MG)">
+                </div>
+                <div class="form-group">
+                  <input type="text" class="form-control" id="" placeholder="Mitsubishi #1 (DE)">
+                </div>
+                <div class="form-group">
+                  <input type="text" class="form-control" id="" placeholder="Mitsubishi #2 (DE)">
+                </div>
+                <div class="form-group">
+                  <span>Sub Total : </span>
+                </div>
+              </form>
+            </div>
+
+            <div class="col-md-6">
+              <h1>create by begus ibrahim</h1>
+            </div>
+    </section>
+</div>
